5d233c00f5694eae029da9354dae7f7ac33aa893
Oxi <33568643+oxi-dev0@users.noreply.github.com>
2022-06-17 00:24:54 +0100

    renamed

--- src/fpacker.cpp
+++ src/fpacker.cpp
@@ -36,7 +36,7 @@ namespace FPacker
 	}
 
 	bool Package::Pack(std::string targetFile) {
-		assert(fromDir != "", "FJoiner: From directory is set");
+		assert(fromDir != "", "FPacker: From directory is set");
 
 		std::vector<std::filesystem::path> foundFiles;
 		for (std::filesystem::recursive_directory_iterator i(fromDir), end; i != end; ++i) {
@@ -45,19 +45,15 @@ namespace FPacker
 			}
 		}
 
-		assert(foundFiles.size() > 0, "FJoiner: The directory has files");
+		assert(foundFiles.size() > 0, "FPacker: The directory has files");
 
 		std::ofstream fileStream(targetFile, std::ios::out | std::ios::binary);
-		char headerBuf[FJOINER_HEADER_LENGTH] = { FJOINER_HEADER };
-		fileStream.write(headerBuf, FJOINER_HEADER_LENGTH);
-
-		/*std::string count = IntToPaddedBinary(foundFiles.size());
-		fileStream.write(count.c_str(), 16);*/
-
+		char headerBuf[FPACKER_HEADER_LENGTH] = { FPACKER_HEADER };
+		fileStream.write(headerBuf, FPACKER_HEADER_LENGTH);
 
 		for (auto& path : foundFiles) {
-			char nameBuf[FJOINER_NAMEMARKER_LENGTH] = { FJOINER_NAMEMARKER };
-			fileStream.write(nameBuf, FJOINER_NAMEMARKER_LENGTH);
+			char nameBuf[FPACKER_NAMEMARKER_LENGTH] = { FPACKER_NAMEMARKER };
+			fileStream.write(nameBuf, FPACKER_NAMEMARKER_LENGTH);
 
 			std::string pathStr = std::filesystem::relative(path, std::filesystem::path(fromDir)).string();
 			fileStream.write(pathStr.c_str(), strlen(pathStr.c_str()));
@@ -70,14 +66,14 @@ namespace FPacker
 				buf.emplace_back(c);
 			}
 
-			char sizeBuf[FJOINER_SIZEMARKER_LENGTH] = { FJOINER_SIZEMARKER };
-			fileStream.write(sizeBuf, FJOINER_SIZEMARKER_LENGTH);
+			char sizeBuf[FPACKER_SIZEMARKER_LENGTH] = { FPACKER_SIZEMARKER };
+			fileStream.write(sizeBuf, FPACKER_SIZEMARKER_LENGTH);
 
 			std::string s = IntToPaddedBinary((int)buf.size());
 			fileStream.write(s.c_str(), 32);
 
-			char fileBuf[FJOINER_FILEMARKER_LENGTH] = { FJOINER_FILEMARKER };
-			fileStream.write(fileBuf, FJOINER_FILEMARKER_LENGTH);
+			char fileBuf[FPACKER_FILEMARKER_LENGTH] = { FPACKER_FILEMARKER };
+			fileStream.write(fileBuf, FPACKER_FILEMARKER_LENGTH);
 
 			for (char d : buf) {
 				char cbuf[1] = { d };
@@ -93,8 +89,8 @@ namespace FPacker
 	bool Package::ValidPackage(std::vector<char> buf) {
 		bool valid = true;
 
-		char headerBuf[FJOINER_HEADER_LENGTH] = { FJOINER_HEADER };
-		for (int i = 0; i < FJOINER_HEADER_LENGTH; i++) {
+		char headerBuf[FPACKER_HEADER_LENGTH] = { FPACKER_HEADER };
+		for (int i = 0; i < FPACKER_HEADER_LENGTH; i++) {
 			if (headerBuf[i] != buf[i]) {
 				valid = false;
 			}
@@ -114,7 +110,7 @@ namespace FPacker
 	}
 
 	bool Package::Unpack(std::string targetDir) {
-		assert(fromFile != "", "FJoiner: From file is set");
+		assert(fromFile != "", "FPacker: From file is set");
 
 		std::vector<char> fileBuf;
 
@@ -125,10 +121,10 @@ namespace FPacker
 			fileBuf.push_back(c);
 		}
 
-		assert(fileBuf.size() > FJOINER_HEADER_LENGTH, "FJoiner: File is valid length");
-		assert(ValidPackage(fileBuf), "FJoiner: File is valid package file");
+		assert(fileBuf.size() > FPACKER_HEADER_LENGTH, "FPacker: File is valid length");
+		assert(ValidPackage(fileBuf), "FPacker: File is valid package file");
 
-		unsigned int cursor = FJOINER_HEADER_LENGTH;
+		unsigned int cursor = FPACKER_HEADER_LENGTH;
 		std::vector<char> nameBuf;
 		std::vector<char> sizeBuf;
 		uint32_t size = 0;
@@ -153,18 +149,18 @@ namespace FPacker
 				}
 
 				nameBuf.clear();
-				cursor += size + FJOINER_NAMEMARKER_LENGTH;
+				cursor += size + FPACKER_NAMEMARKER_LENGTH;
 
 				section = 0;
 				continue;
 			} break;
 			case 1: // size section
 			{
-				if (LookaheadMatch(fileBuf, cursor, { FJOINER_FILEMARKER })) {
+				if (LookaheadMatch(fileBuf, cursor, { FPACKER_FILEMARKER })) {
 					size = PaddedBinaryToInt(VectorToString(sizeBuf).str());
 
 					section = 2;
-					cursor += FJOINER_FILEMARKER_LENGTH;
+					cursor += FPACKER_FILEMARKER_LENGTH;
 					continue;
 				}
 				else {
@@ -176,12 +172,12 @@ namespace FPacker
 			} break;
 			case 0: // name section
 			{
-				if (LookaheadMatch(fileBuf, cursor, { FJOINER_SIZEMARKER })) {
+				if (LookaheadMatch(fileBuf, cursor, { FPACKER_SIZEMARKER })) {
 					sizeBuf.clear();
 					dataBuf.clear();
 
 					section = 1;
-					cursor += FJOINER_SIZEMARKER_LENGTH;
+					cursor += FPACKER_SIZEMARKER_LENGTH;
 					continue;
 				}
 				else {
